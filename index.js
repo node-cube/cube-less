00174a5eb8a20abe34d34d8b34121f35bbe564b7
剪巽 <jianxun.zxl@taobao.com>
2015-11-05 00:26:29 +0800

    update less

--- index.js
+++ index.js
@@ -16,12 +16,42 @@ LessProcessor.prototype = {
     var absFile = path.join(root, file);
     var code = fs.readFileSync(absFile).toString();
     var codeRes;
+    /*
     var lessParser = new(less.Parser)({
       paths: [path.dirname(absFile), root]
     });
+    */
     var self = this;
+    less.render(
+      code,
+      {
+        paths: [path.dirname(absFile), root],
+        compress: options.compress
+      },
+      function (err, data) {
+        if (err) {
+          err.code = 'Css_Parse_Error';
+          err.message = 'File:' + file + ', Line:' + err.line + ', Column:' + err.column + ' ' + err.message + ' extract:\n' + err.extract.join('\n');
+          return callback(err);
+        }
+        var res = {
+          source: code,
+          code: data.css
+        };
+        if (self.cube.fixupResPath) {
+          res.code = self.cube.fixupResPath(path.dirname(options.qpath), res.code);
+        }
+        if (options.moduleWrap) {
+          res.wraped = self.cube.wrapStyle(options.qpath, res.code);
+        }
+        callback(null, res);
+      }
+    );
+    /*
     lessParser.parse(code, function (err, tree) {
       if (err) {
+        err.code = 'Css_Parse_Error';
+        err.message = 'File:' + file + ', Line:' + err.line + ', Column:' + err.column + ' ' + err.message + ' extract:\n' + err.extract.join('\n');
         return callback(err);
       }
       codeRes = tree.toCSS({
@@ -40,6 +70,7 @@ LessProcessor.prototype = {
       }
       callback(null, res);
     });
+    */
   }
 };
 
