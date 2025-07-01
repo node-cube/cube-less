82ceed5f7d98fb91bad47279504378a758bc2664
elvira.zy <elvira.zy@alibaba-inc.com>
2025-07-01 17:40:40 +0800

    fix: 用cube自带通用逻辑解决 less 文件中的 import

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
 var path = require('path');
 var less = require('less');
 
+class CustomFileManager extends less.FileManager {
+  constructor(cube, data) {
+    super();
+    this.cube = cube;
+    this.data = data;
+  }
+  loadFile(filename, currentDirectory, options, environment) {
+    // 判断是否为绝对路径或相对路径
+    if (path.isAbsolute(filename) || filename.startsWith('.') || filename.startsWith('..')) {
+      return super.loadFile(filename, currentDirectory, options, environment);
+    }
+    // 判断是否为库名（@开头或字母开头）
+    if (/^@?\w+/.test(filename)) {
+      // 走 cube 的 getFile 逻辑
+      if (this.cube && typeof this.cube.readFile === 'function') {
+        return new Promise((resolve, reject) => {
+          this.cube.resolveModulePath(this.data, filename, (err, { path, code } = {}) => {
+            if (err || code === 1) {
+              // 错误的话还是走默认 less 的逻辑
+              super.loadFile(filename, currentDirectory, options, environment).then(resolve, reject);
+            } else {
+              const data = {
+                queryPath: path,
+                realPath: filename
+              };
+              this.cube.readFile(data, (err, fileData) => {
+                if (err || !fileData || !fileData.code) {
+                  super.loadFile(filename, currentDirectory, options, environment).then(resolve, reject);
+                } else {
+                  resolve({
+                    filename: fileData.realPath || filename,
+                    contents: fileData.code
+                  });
+                }
+              });
+            }
+          });
+        });
+      }
+    }
+    return super.loadFile(filename, currentDirectory, options, environment);
+  }
+}
 function LessProcessor(cube, config) {
   this.cube = cube;
   this.config = config || {};
@@ -14,6 +57,8 @@ LessProcessor.prototype.process = function (data, callback) {
   var config = this.cube.config;
   var file = path.join(config.root, data.realPath);
   var self = this;
+
+  const customFileManager = new CustomFileManager(this.cube, data);
   less.render(
     code,
     {
@@ -21,7 +66,12 @@ LessProcessor.prototype.process = function (data, callback) {
       strictMath: true,
       paths: [path.dirname(file), config.root, path.join(config.root, 'node_modules')],
       modifyVars: {}, // less中的变量表
-      compress: data.compress || config.compress
+      compress: data.compress || config.compress,
+      plugins: [{
+        install: function (lessInstance, pluginManager) {
+          pluginManager.addFileManager(customFileManager);
+        }
+      }]
     },
     function (err, result) {
       if (err) {
